1e0204e228d55e5ee561c11754b202f1d4ff55aa
roberttripleh <roby3h@gmail.com>
2025-02-13 21:18:45 +0200

    first lambda

--- Source/Aura/Private/AbilitySystem/PsychAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/PsychAbilitySystemComponent.cpp
@@ -13,10 +13,7 @@ void UPsychAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Abilit
 {
 	FGameplayTagContainer TagContainer;
 	EffectSpec.GetAllAssetTags(TagContainer);
-	for (const FGameplayTag& Tag : TagContainer)
-	{
-		//TODO: Broadcast the tag to the Widget Controller
-		const FString Msg = FString::Printf(TEXT("GE Tag: %s"),*Tag.ToString());
-		GEngine->AddOnScreenDebugMessage(-1,8.f,FColor::Blue, Msg);
-	}
+	EffectAssetTags.Broadcast(TagContainer);
+	
+
 }
--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,6 +3,7 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+#include "AbilitySystem/PsychAbilitySystemComponent.h"
 #include "AbilitySystem/PsychAttributeSet.h"
 
 void UOverlayWidgetController::BroadcastInitialValues()
@@ -31,6 +32,17 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
 		PsychAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
+
+	Cast<UPsychAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
+	[](const FGameplayTagContainer& AssetTags)
+	{
+		for (const FGameplayTag& Tag : AssetTags)
+		{
+			const FString Msg = FString::Printf(TEXT("GE Tag: %s"),*Tag.ToString());
+			GEngine->AddOnScreenDebugMessage(-1,8.f,FColor::Blue, Msg);
+		} 
+	}
+	);
 }
 
 void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
