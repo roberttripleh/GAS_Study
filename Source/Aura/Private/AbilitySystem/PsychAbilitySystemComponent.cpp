cfd3cf0305c616ddeb022a12be56c6bca33b3651
roberttripleh <roby3h@gmail.com>
2025-03-08 18:29:29 +0200

    created tags

--- Source/Aura/Private/AbilitySystem/PsychAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/PsychAbilitySystemComponent.cpp
@@ -10,13 +10,6 @@ void UPsychAbilitySystemComponent::AbilityActorInfoSet()
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this,&UPsychAbilitySystemComponent::EffectApplied);
 
 	const FPsychGameplayTags& GameplayTags = FPsychGameplayTags::Get();
-
-	GEngine->AddOnScreenDebugMessage(
-		-1,
-		10.f,
-		FColor::Orange,
-		FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString()));
-
 	
 }
 
